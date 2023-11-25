b9d706a493f76570f9646ecabb7dc5c3d805a521
Sam Morgen <samorgen14@gmail.com>
2023-11-25 13:34:01 -0800

    Profile image

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Image } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: '#348feb', fontWeight: 'bold', fontSize: 30 }}>
+        Edit Profile
+      </Text>
+      <Image
+        source={require('./assets/profile.jpg')}
+        style={{
+          height: 150,
+          width: 150,
+          borderRadius: 150 / 2,
+          overflow: 'hidden',
+          borderWidth: 3,
+          borderColor: '#348feb'
+        }}
+      />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +28,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
