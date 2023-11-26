8b002d34a4461841d06c4aff75986e140ab10759
Sam Morgen <samorgen14@gmail.com>
2023-11-26 10:30:50 -0800

    Photo upload page

--- screens/Avatar.js
+++ screens/Avatar.js
@@ -1,7 +1,37 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { Text, View, Image, Pressable } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 const Avatar = () => {
-  return <Text>Avatar Edit</Text>
+  const navigation = useNavigation()
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontWeight: 'bold', fontSize: 30 }}>
+        Upload a photo of yourself
+      </Text>
+      <Image
+        source={require('../assets/profile.jpg')}
+        style={{
+          width: '80%',
+          margin: 40
+        }}
+      />
+      {/* <Button title='Update' color='#f194ff' /> */}
+      <Pressable
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingVertical: 12,
+          paddingHorizontal: 32,
+          elevation: 3,
+          backgroundColor: 'black',
+          width: '80%'
+        }}
+        onPress={() => navigation.navigate('Profile')}
+      >
+        <Text style={{ color: 'white' }}>Update</Text>
+      </Pressable>
+    </View>
+  )
 }
 
 export default Avatar
