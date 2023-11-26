01fa066565ba88cb957f8c79960e57477105aa3d
Sam Morgen <samorgen14@gmail.com>
2023-11-26 11:43:26 -0800

    Component for profile info

---
+++ components/ProfileInfo.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  ImageBackground,
+  TextInput
+} from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { IconButton } from 'react-native-paper'
+
+const ProfileInfo = ({ infoTitle, infoValue, enableMultiLine, numOfLines }) => {
+  const navigation = useNavigation()
+  return (
+    <View style={{ width: '75%', marginTop: 20 }}>
+      <View
+        style={{
+          borderBottomColor: '#000000',
+          borderBottomWidth: 1
+        }}
+      >
+        <Text
+          style={
+            enableMultiLine
+              ? { marginBottom: 0, color: 'gray' }
+              : { marginBottom: -10, color: 'gray' }
+          }
+        >
+          {infoTitle}
+        </Text>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between'
+          }}
+        >
+          <TextInput
+            multiline={enableMultiLine}
+            numberOfLines={numOfLines}
+            maxLength={40}
+            //onChangeText={(text) => onChangeText(text)}
+            value={infoValue}
+            style={{ fontWeight: 'bold' }}
+          />
+          <IconButton
+            icon='chevron-right'
+            onPress={() => navigation.navigate('Edit Avatar')}
+          />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default ProfileInfo
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,6 +11,7 @@ import {
 } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { IconButton } from 'react-native-paper'
+import ProfileInfo from '../components/ProfileInfo'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
@@ -41,37 +42,17 @@ const HomeScreen = () => {
         />
       </View>
 
-      <View style={{ width: '75%', marginTop: 20 }}>
-        <View
-          style={{
-            borderBottomColor: '#000000',
-            borderBottomWidth: 1
-          }}
-        >
-          <Text style={{ marginBottom: -10, color: 'gray' }}>Name</Text>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between'
-            }}
-          >
-            <TextInput
-              // editable
-              // multiline
-              // numberOfLines={4}
-              maxLength={40}
-              //onChangeText={(text) => onChangeText(text)}
-              value={'Sam Morgen'}
-              style={{ fontWeight: 'bold' }}
-            />
-            <IconButton
-              icon='chevron-right'
-              onPress={() => navigation.navigate('Edit Avatar')}
-            />
-          </View>
-        </View>
-      </View>
+      <ProfileInfo infoTitle={'Name'} infoValue={'Sam Morgen'} />
+      <ProfileInfo infoTitle={'Phone'} infoValue={'000-000-0000'} />
+      <ProfileInfo infoTitle={'Email'} infoValue={'test@test.com'} />
+      <ProfileInfo
+        infoTitle={'Tell us about yourself'}
+        infoValue={
+          'asdf adsf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf '
+        }
+        numOfLines={4}
+        enableMultiLine={true}
+      />
 
       <StatusBar style='auto' />
     </View>
