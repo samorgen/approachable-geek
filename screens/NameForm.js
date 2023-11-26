f32e23bb718e291f11ac1db9ff1d7282a2ec900f
Sam Morgen <samorgen14@gmail.com>
2023-11-26 12:09:08 -0800

    Text input for name form

--- screens/NameForm.js
+++ screens/NameForm.js
@@ -1,4 +1,4 @@
-import { Text, View, Image, Pressable } from 'react-native'
+import { Text, View, Image, Pressable, TextInput } from 'react-native'
 import UpdateButton from '../components/UpdateButton'
 
 const NameForm = () => {
@@ -7,6 +7,38 @@ const NameForm = () => {
       <Text style={{ fontWeight: 'bold', fontSize: 30 }}>
         What's your name?
       </Text>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          width: '75%',
+          justifyContent: 'space-between'
+        }}
+      >
+        <View
+          style={{
+            borderWidth: 1,
+            borderColor: 'gray',
+            minWidth: 100,
+            minHeight: 50
+          }}
+        >
+          <Text style={{ color: 'gray', fontWeight: 'bold' }}>First Name</Text>
+          <TextInput placeholder='John' />
+        </View>
+        <View
+          style={{
+            borderWidth: 1,
+            borderColor: 'gray',
+            minWidth: 100,
+            minHeight: 50
+          }}
+        >
+          <Text style={{ color: 'gray', fontWeight: 'bold' }}>Last Name</Text>
+          <TextInput placeholder='Doe' />
+        </View>
+      </View>
+
       <UpdateButton />
     </View>
   )
